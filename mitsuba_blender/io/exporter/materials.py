753809f0509425b07968bbaaae64a7850d52116c
Peter Kocsis <peter.kocsis@tum.de>
2022-12-21 12:34:34 +0100

    Handle emission textures

--- mitsuba_blender/io/exporter/materials.py
+++ mitsuba_blender/io/exporter/materials.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from mathutils import Matrix
 from .export_context import Files
@@ -159,25 +161,44 @@ def convert_glass_materials_cycles(export_ctx, current_node):
     return params
 
 def convert_emitter_materials_cycles(export_ctx, current_node):
-
-    if  current_node.inputs["Strength"].is_linked:
+    if current_node.inputs["Strength"].is_linked:
         raise NotImplementedError("Only default emitter strength value is supported.")#TODO: value input
 
     else:
         radiance = current_node.inputs["Strength"].default_value
+        params_radiance = export_ctx.spectrum(radiance)
 
     if current_node.inputs['Color'].is_linked:
-        raise NotImplementedError("Only default emitter color is supported.")#TODO: rgb input
+        color_node = current_node.inputs['Color'].links[0].from_node
+        if current_node.inputs['Strength'].is_linked:
+            raise NotImplementedError("Only default strength is supported.")  # TODO: rgb input
+        else:
+            strength = current_node.inputs['Strength'].default_value
+
+        if color_node.name == 'Blackbody':
+            # Blackbody is supported only in spectral mode
+            # -> Approximate it by single color emission
+            # TODO: Consider the strength!
+            temperature = color_node.inputs['Temperature'].default_value
+            radiance = [float(intensity) / 255 * strength for intensity in convert_K_to_RGB(temperature)]
+            params_radiance = export_ctx.spectrum(radiance)
+        elif color_node.bl_idname == "ShaderNodeTexImage":
+            # TODO: Implement emission texture
+            filename = color_node.image.filepath
+            params_radiance = {"type": "bitmap", "filename": filename}
+        else:
+            raise NotImplementedError("Only default emitter color is supported.")  # TODO: rgb input
 
     else:
         radiance = [x * radiance for x in current_node.inputs["Color"].default_value[:]]
         if np.sum(radiance) == 0:
             export_ctx.log("Emitter has zero emission, this will case mitsuba to fail! Ignoring it.", 'WARN')
             return {'type':'diffuse', 'reflectance': export_ctx.spectrum(0)}
+        params_radiance = export_ctx.spectrum(radiance)
 
     params = {
         'type': 'area',
-        'radiance': export_ctx.spectrum(radiance),
+        'radiance': params_radiance,
     }
 
     return params
@@ -504,6 +525,9 @@ def convert_world(export_ctx, world, ignore_background):
             raise NotImplementedError("Only Background and Emission nodes are supported as final nodes for World export, got '%s'" % surface_node.name)
     else:
         # Single color field for emission, no nodes
+        if ignore_background:
+            export_ctx.log("Ignoring Blender's default background...", 'INFO')
+            return
         params.update({
             'type': 'constant',
             'radiance': export_ctx.spectrum(world.color)
@@ -525,3 +549,64 @@ def export_world(export_ctx, world, ignore_background):
         convert_world(export_ctx, world, ignore_background)
     except NotImplementedError as err:
         export_ctx.log("Error while exporting world: %s. Not exporting it." % err.args[0], 'WARN')
+
+
+def convert_K_to_RGB(colour_temperature):
+    """
+    Converts from K to RGB, algorithm courtesy of
+    http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+    """
+    # range check
+    if colour_temperature < 1000:
+        colour_temperature = 1000
+    elif colour_temperature > 40000:
+        colour_temperature = 40000
+
+    tmp_internal = colour_temperature / 100.0
+
+    # red
+    if tmp_internal <= 66:
+        red = 255
+    else:
+        tmp_red = 329.698727446 * math.pow(tmp_internal - 60, -0.1332047592)
+        if tmp_red < 0:
+            red = 0
+        elif tmp_red > 255:
+            red = 255
+        else:
+            red = tmp_red
+
+    # green
+    if tmp_internal <= 66:
+        tmp_green = 99.4708025861 * math.log(tmp_internal) - 161.1195681661
+        if tmp_green < 0:
+            green = 0
+        elif tmp_green > 255:
+            green = 255
+        else:
+            green = tmp_green
+    else:
+        tmp_green = 288.1221695283 * math.pow(tmp_internal - 60, -0.0755148492)
+        if tmp_green < 0:
+            green = 0
+        elif tmp_green > 255:
+            green = 255
+        else:
+            green = tmp_green
+
+    # blue
+    if tmp_internal >= 66:
+        blue = 255
+    elif tmp_internal <= 19:
+        blue = 0
+    else:
+        tmp_blue = 138.5177312231 * math.log(tmp_internal - 10) - 305.0447927307
+        if tmp_blue < 0:
+            blue = 0
+        elif tmp_blue > 255:
+            blue = 255
+        else:
+            blue = tmp_blue
+
+    return red, green, blue
+
